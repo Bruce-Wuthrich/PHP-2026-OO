4f4de3ed7c9f3889df5221cc42c36a643608b7b4
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-09-14 10:25:50 +0200

    miau!

--- Classe2/bruce/chapitre-01/chat.php
+++ Classe2/bruce/chapitre-01/chat.php
@@ -4,15 +4,22 @@ class Chat
 {
     public string $nom = 'Sans nom';
     public int $age = 0;
+    public function miauler(): string
+    {
+        return 'Miaou !';
+    }
+
 }
 
 $chat = new Chat();
 
  $chat->nom = 'Félix'. PHP_EOL;
  $chat->age ='3' . PHP_EOL;
-
- echo "$chat->nom <br> $chat->age";
+ $chat->miauler() .PHP_EOL;
+ echo "$chat->nom <br> $chat->age <br>";
+ echo $chat->miauler() .PHP_EOL;
  
+
  
 
 

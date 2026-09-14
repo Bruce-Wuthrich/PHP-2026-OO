2e520eeac6b897ca0450bfcba58d2c6980891b28
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-09-14 11:22:51 +0200

    exercices 1.8 fini start du bonus

--- Classe2/bruce/chapitre-01/chat.php
+++ Classe2/bruce/chapitre-01/chat.php
@@ -2,24 +2,108 @@
 
 class Chat
 {
-    public string $nom = 'Sans nom';
-    public int $age = 0;
+    public function __construct(
+        public string $nom,
+        private int $age,
+    ) {}
+
     public function miauler(): string
     {
         return 'Miaou !';
     }
+        public function sePresenter(): string
+    {
+        return 'Je suis ' . $this->nom . ' et j\'ai ' . $this->age . ' ans.';
+    }
+        public function getAge(): int
+    {
+        return $this->age;
+    }
+        public function setAge(int $age): void
+    {
+        if ($age < 0) {
+            echo '⛔ Un âge négatif ? Non. On garde ' . $this->age . '.' . PHP_EOL;
+            return;
+        }
+
+        $this->age = $age;
+    }
 
 }
 
-$chat = new Chat();
+$chat = new Chat('Félix', 3);
 
- $chat->nom = 'Félix'. PHP_EOL;
- $chat->age ='3' . PHP_EOL;
+$chat2 = new Chat('Grosminet', 5);
  $chat->miauler() .PHP_EOL;
- echo "$chat->nom <br> $chat->age <br>";
- echo $chat->miauler() .PHP_EOL;
- 
+ $chat->sePresenter().PHP_EOL;
+  echo "$chat->nom" .PHP_EOL;
+echo $chat->getAge() .'<br>';
+  echo $chat->miauler() . '<br>';
+ echo $chat->sePresenter() . '<br>';
+echo $chat2->sePresenter() . '<br>';
+echo  $chat->getAge() . '<br>';
+$chat->setAge(4);
+echo $chat->getAge() . '<br>' ;   // 4
+
+$chat->setAge(-50);               // refusé !
+echo $chat->getAge() . '<br>' ;   // toujours 4
+
+
 
  
+class Tamagotchi
+{
+    public function __construct(
+        private string $nom,
+    ) {} 
+    private int $faim = 50;
+
+ public function getNom(): string
+    {
+        return $this->nom;
+
+    }
+
+         public function getFaim(): int
+    {
+        return $this->faim;
+    }
+
+    public function manger(): void
+    {
+        $this->faim = $this->faim - 20;
+    }
+
+    public function jouer(): void
+    {
+        $this->faim = $this->faim + 15;
+    }
+
+    public function etat(): string
+    {
+        return '🐣 ' . $this->nom . ' a une faim de ' . $this->faim . '/100';
+    }
+
+}
+
+
+$pixel = new Tamagotchi('Pixel');
+
+echo $pixel->etat() . '<br>';
+
+$pixel->manger();
+echo $pixel->etat() . '<br>';
+
+$pixel->jouer();
+echo $pixel->etat() . '<br>';
+
+
+
+
+
+
+
+
+
 
 
